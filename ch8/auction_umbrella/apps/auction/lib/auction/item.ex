377eafac9dad832b6c51c8bad79041955d4d9680
JP <jp.bonner@bonner.jp>
2021-08-10 22:39:46 -0400

    ch8

--- ch8/auction_umbrella/apps/auction/lib/auction/item.ex
+++ ch8/auction_umbrella/apps/auction/lib/auction/item.ex
@@ -13,5 +13,15 @@ defmodule Auction.Item do
     item
     |> cast(params, [:title, :description, :ends_at])
     |> validate_required([:title])
+    |> validate_length(:title, min: 3)
+    |> validate_length(:description, max: 200)
+    |> validate_change(:ends_at, &validate/2)
+  end
+
+  defp validate(:ends_at, ends_at_date) do
+    case DateTime.compare(ends_at_date, DateTime.utc_now()) do
+      :lt -> [ends_at: "end date cannot be in the past"]
+      _ -> []
+    end
   end
 end
