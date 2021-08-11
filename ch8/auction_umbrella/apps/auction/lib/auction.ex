b31af6e2e2025124999d284f9a9499e59fcfc431
JP <jp.bonner@bonner.jp>
2021-08-11 12:25:07 -0400

    complete ch8

--- ch8/auction_umbrella/apps/auction/lib/auction.ex
+++ ch8/auction_umbrella/apps/auction/lib/auction.ex
@@ -17,11 +17,20 @@ defmodule Auction do
     @repo.get_by(Item, attrs)
   end
 
+  @spec insert_item(Map) :: {:ok, Item} | {:error, struct}
   def insert_item(attrs) do
-    Auction.Item
+    Item
     |> struct(attrs)
     |> @repo.insert()
   end
 
+  @spec update_item(Item, Map) :: {:ok, Item} | {:error, Ecto.Changeset}
+  def update_item(%Item{} = item, attrs) do
+    item
+    |> Item.changeset(attrs)
+    |> @repo.update()
+  end
+
+  @spec delete_item(Item) :: {:ok, Item} | {:error, Ecto.Changeset}
   def delete_item(%Auction.Item{} = item), do: @repo.delete(item)
 end
