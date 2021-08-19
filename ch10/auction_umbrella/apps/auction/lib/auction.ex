6315159d0c5180f009554fbd296a3a570cfb5e84
JP <jp.bonner@bonner.jp>
2021-08-19 19:18:02 -0400

    ip: user creation

--- ch10/auction_umbrella/apps/auction/lib/auction.ex
+++ ch10/auction_umbrella/apps/auction/lib/auction.ex
@@ -1,5 +1,5 @@
 defmodule Auction do
-  alias Auction.{Repo, Item}
+  alias Auction.{Repo, Item, User}
   @repo Auction.Repo
 
   @spec list_items :: [Item]
@@ -42,4 +42,14 @@ defmodule Auction do
 
   @spec delete_item(Item) :: {:ok, Item} | {:error, Ecto.Changeset}
   def delete_item(%Auction.Item{} = item), do: @repo.delete(item)
+
+  def get_user(id), do: @repo.get!(User, id)
+
+  def new_user, do: User.changeset_with_password(%User{})
+
+  def insert_user(params) do
+    %User{}
+    |> User.changeset_with_password(params)
+    |> @repo.insert
+  end
 end
--- ch10/auction_umbrella/apps/auction/mix.exs
+++ ch10/auction_umbrella/apps/auction/mix.exs
@@ -28,7 +28,9 @@ defmodule Auction.MixProject do
     [
       {:ecto_sql, "~> 3.6.2"},
       {:postgrex, "~> 0.15.9"},
-      {:uuid, "~> 1.1.8"}
+      {:uuid, "~> 1.1.8"},
+      {:comeonin, "~> 5.3.2"},
+      {:pbkdf2_elixir, "~> 1.4.1"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true}
