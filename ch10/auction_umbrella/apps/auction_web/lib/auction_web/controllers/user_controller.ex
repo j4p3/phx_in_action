e8fc6dd7e8c9301072b2d9172cb2c1c27b4f4f71
JP <jp.bonner@bonner.jp>
2021-08-19 19:18:26 -0400

    ip: user creation

---
+++ ch10/auction_umbrella/apps/auction_web/lib/auction_web/controllers/user_controller.ex
@@ -0,0 +1,15 @@
+defmodule Auction.UserController do
+  use AuctionWeb, :controller
+
+  def show(conn, %{"id" => id}) do
+
+  end
+
+  def new(conn, _params) do
+
+  end
+
+  def create(conn, %{"user" => user_params}) do
+
+  end
+end
